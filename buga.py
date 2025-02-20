b44f2cac00e6e406f6ab70e020a9a5d5bd3b2f79
Veldar115 <iamhero114@gmail.com>
2025-02-20 15:30:27 +0500

    Spagetty code

--- buga.py
+++ buga.py
@@ -3,7 +3,9 @@ User_Answers = []
 #Right_Answers = ["Put","Swam","Built","Woken","Slept","Played", "Seen", "Run"]
 Right_Answers = ["Put","Swam"]
 def Render_Result():
+    global frame
     Procent_Right_Answers = Check_Answers(User_Answers, Right_Answers)
+    print(Procent_Right_Answers)
     frame = tkinter.Frame(borderwidth=1, relief="solid")
     Label = tkinter.Label(frame, text=f"{Procent_Right_Answers}", background="#FFCDD2", foreground="#B71C1C")
     Label.place(x=600, y = 100)
@@ -12,15 +14,18 @@ def Render_Result():
 def Save_Answers(Answer):
     User_Answers.append(Answer)
 def Next_Question(Answer, Previous_Question_Number):
+    global frame
     Save_Answers(Answer)
     clear_all_inside_frame()
     Current_Question_Number = Previous_Question_Number + 1
-    if Current_Question_Number >= len(Right_Answers):
-        Render_Result()
+    if Current_Question_Number == len(Right_Answers):
+        print("Пытаюсь вывести результат")
+        frame = Render_Result()
     else:
         render_frame_question_with_options(Current_Question_Number)
     print(User_Answers)
 def render_frame_question_with_options(Question_Number):
+    global frame
     Answers = list(Questiions.values())[Question_Number]
     frame = tkinter.Frame(borderwidth=1, relief="solid")
     Label = tkinter.Label(frame, text=f"{list(Questiions.keys())[Question_Number]}", background="#FFCDD2", foreground="#B71C1C")
@@ -35,7 +40,7 @@ def render_frame_question_with_options(Question_Number):
             #некоректно
             btn.grid(row=r, column=c, ipadx=6, ipady=6, padx=4, pady=4, sticky="sew")
             I_Index += 1
-    #frame.pack(anchor="center", padx=5, pady=5, expand=True)
+    frame.pack(anchor="center", padx=5, pady=5, expand=True)
     return frame
 def Check_Answers(User_Answers, Right_Answers):
     a = 0
@@ -53,10 +58,10 @@ Questiions = {"Second form of PUT":("Put","Putten","Pot","Pyt"),
                "Second form of Swim":("Swam","Swimmen", "Swum", "Swim"),
                  "Third form of Build":("Boult","Built", "Builden", "Boulght"),
                    "Second form of Wake":("Woke","Woken", "Wokn", "Wake"),
-                   "Third form of Sleep":("Sleep","Slep", "Slept", "Sleepen"),
+                    "Third form of Sleep":("Sleep","Slep", "Slept", "Sleepen"),
                      "Second form of Play":("Played","Playn", "Play", "Pluy"),
-                       "Third form of See":("Saw","Seen", "See", "Seenen"),
-                         "Third form of Run":("Runnen","Ran", "Run", "Runn")}
+                      "Third form of See":("Saw","Seen", "See", "Seenen"),
+                       "Third form of Run":("Runnen","Ran", "Run", "Runn")}
 Root = tkinter.Tk() 
 Root.title()
 Root.geometry("1200x1200")
